1d58a665240edd5c2896c5c641bf32de6b75659a
MarceloHFilho <contato@omarceleza.com.br>
2023-10-15 21:44:54 -0300

    Add certs

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,23 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
+import * as fs from "fs";
+import * as path from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  server: {
+    cors: {
+      origin: "*",
+    },
+    https: {
+      key: fs.readFileSync("./.docker/nginx/certs/localhost-key.pem"),
+      cert: fs.readFileSync("./.docker/nginx/certs/localhost.pem"),
+    },
+  },
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
 })
